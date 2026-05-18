6c30ffc34852a20d063e8364d0ebfd7de1f55288
Islam Talipov <italipov2022@gmail.com>
2026-05-18 18:32:14 +0300

    new highlight

--- rzk/src/Language/Rzk/VSCode/Tokenize.hs
+++ rzk/src/Language/Rzk/VSCode/Tokenize.hs
@@ -14,34 +14,50 @@ tokenizeModule (Module _loc langDecl commands) = concat
   ]
 
 tokenizeLanguageDecl :: LanguageDecl -> [SemanticTokenAbsolute]
-tokenizeLanguageDecl _ = []
+tokenizeLanguageDecl ld@(LanguageDecl _ _) = mkKeyword ld "#lang"
 
 tokenizeCommand :: Command -> [SemanticTokenAbsolute]
 tokenizeCommand command = case command of
-  CommandSetOption{}   -> []    -- NOTE: fallback to TextMate
-  CommandUnsetOption{} -> []    -- NOTE: fallback to TextMate
-  CommandCheck        _loc term type_ -> foldMap tokenizeTerm [term, type_]
-  CommandCompute      _loc term -> tokenizeTerm term
-  CommandComputeNF    _loc term -> tokenizeTerm term
-  CommandComputeWHNF  _loc term -> tokenizeTerm term
+  cmd@CommandSetOption{}   -> mkKeyword cmd "#set-option"
+  cmd@CommandUnsetOption{} -> mkKeyword cmd "#unset-option"
 
-  CommandPostulate _loc name _declUsedVars params type_ -> concat
-    [ mkToken name SemanticTokenTypes_Function [SemanticTokenModifiers_Declaration]
+  cmd@(CommandCheck _ term type_) -> concat
+    [ mkKeyword cmd "#check"
+    , foldMap tokenizeTerm [term, type_]
+    ]
+  cmd@(CommandCompute _ term) -> concat
+    [ mkKeyword cmd "#compute"
+    , tokenizeTerm term
+    ]
+  cmd@(CommandComputeNF _ term) -> concat
+    [ mkKeyword cmd "#compute-nf"
+    , tokenizeTerm term
+    ]
+  cmd@(CommandComputeWHNF _ term) -> concat
+    [ mkKeyword cmd "#compute-whnf"
+    , tokenizeTerm term
+    ]
+
+  cmd@(CommandPostulate _ name _declUsedVars params type_) -> concat
+    [ mkKeyword cmd "#postulate"
+    , mkToken name SemanticTokenTypes_Function [SemanticTokenModifiers_Declaration]
     , foldMap tokenizeParam params
     , tokenizeTerm type_
     ]
-  CommandDefine _loc name _declUsedVars params type_ term -> concat
-    [ mkToken name SemanticTokenTypes_Function [SemanticTokenModifiers_Declaration]
+  cmd@(CommandDefine _ name _declUsedVars params type_ term) -> concat
+    [ mkKeyword cmd "#def"
+    , mkToken name SemanticTokenTypes_Function [SemanticTokenModifiers_Declaration]
     , foldMap tokenizeParam params
     , foldMap tokenizeTerm [type_, term]
     ]
 
-  CommandAssume _loc vars type_ -> concat
-    [ foldMap (\var -> mkToken var SemanticTokenTypes_Parameter [SemanticTokenModifiers_Declaration]) vars
+  cmd@(CommandAssume _ vars type_) -> concat
+    [ mkKeyword cmd "#assume"
+    , foldMap (\var -> mkToken var SemanticTokenTypes_Parameter []) vars
     , tokenizeTerm type_
     ]
-  CommandSection    _loc _nameStart -> []
-  CommandSectionEnd _loc _nameEnd -> []
+  cmd@CommandSection{}    -> mkKeyword cmd "#section"
+  cmd@CommandSectionEnd{} -> mkKeyword cmd "#end"
 
 tokenizeBind :: Bind -> [SemanticTokenAbsolute]
 tokenizeBind = \case
@@ -62,8 +78,9 @@ tokenizeParam = \case
     [ tokenizePattern pat
     , tokenizeTerm cube
     , tokenizeTope tope ]
-  ParamPatternModalType _loc pats _md ty -> concat
+  ParamPatternModalType _loc pats md ty -> concat
     [ foldMap tokenizePattern pats
+    , tokenizeModality md
     , tokenizeTerm ty ]
 
 tokenizePattern :: Pattern -> [SemanticTokenAbsolute]
@@ -105,9 +122,9 @@ tokenizeTerm' varTokenType = go
       CubeProduct _loc l r -> foldMap go [l, r]
 
       TopeTop{}            -> mkToken term SemanticTokenTypes_String [SemanticTokenModifiers_DefaultLibrary]
-      ASCII_TopeTop{}            -> mkToken term SemanticTokenTypes_String [SemanticTokenModifiers_DefaultLibrary]
+      ASCII_TopeTop{}      -> mkToken term SemanticTokenTypes_String [SemanticTokenModifiers_DefaultLibrary]
       TopeBottom{}         -> mkToken term SemanticTokenTypes_String [SemanticTokenModifiers_DefaultLibrary]
-      ASCII_TopeBottom{}         -> mkToken term SemanticTokenTypes_String [SemanticTokenModifiers_DefaultLibrary]
+      ASCII_TopeBottom{}   -> mkToken term SemanticTokenTypes_String [SemanticTokenModifiers_DefaultLibrary]
       TopeAnd _loc l r     -> foldMap tokenizeTope [l, r]
       ASCII_TopeAnd _loc l r     -> foldMap tokenizeTope [l, r]
       TopeOr  _loc l r     -> foldMap tokenizeTope [l, r]
@@ -131,27 +148,29 @@ tokenizeTerm' varTokenType = go
         [ tokenizeParamDecl paramDecl
         , go ret ]
       TypeSigma loc pat a b -> concat
-        [ mkToken (VarIdent loc "∑") SemanticTokenTypes_Class [SemanticTokenModifiers_DefaultLibrary]
+        [ mkToken (VarIdent loc "∑") SemanticTokenTypes_Type [SemanticTokenModifiers_DefaultLibrary]
         , tokenizePattern pat
         , foldMap go [a, b] ]
-      TypeSigmaModal loc pat _md a b -> concat
-        [ mkToken (VarIdent loc "∑") SemanticTokenTypes_Class [SemanticTokenModifiers_DefaultLibrary]
+      TypeSigmaModal loc pat md a b -> concat
+        [ mkToken (VarIdent loc "∑") SemanticTokenTypes_Type [SemanticTokenModifiers_DefaultLibrary]
         , tokenizePattern pat
+        , tokenizeModality md
         , foldMap go [a, b] ]
       ASCII_TypeSigma loc pat a b -> concat
-        [ mkToken (VarIdent loc "Sigma") SemanticTokenTypes_Class [SemanticTokenModifiers_DefaultLibrary]
+        [ mkToken (VarIdent loc "Sigma") SemanticTokenTypes_Type [SemanticTokenModifiers_DefaultLibrary]
         , tokenizePattern pat
         , foldMap go [a, b] ]
-      ASCII_TypeSigmaModal loc pat _md a b -> concat
-        [ mkToken (VarIdent loc "Sigma") SemanticTokenTypes_Class [SemanticTokenModifiers_DefaultLibrary]
+      ASCII_TypeSigmaModal loc pat md a b -> concat
+        [ mkToken (VarIdent loc "Sigma") SemanticTokenTypes_Type [SemanticTokenModifiers_DefaultLibrary]
         , tokenizePattern pat
+        , tokenizeModality md
         , foldMap go [a, b] ]
       TypeSigmaTuple loc p ps tN -> concat
-        [ mkToken (VarIdent loc "∑") SemanticTokenTypes_Class [SemanticTokenModifiers_DefaultLibrary]
+        [ mkToken (VarIdent loc "∑") SemanticTokenTypes_Type [SemanticTokenModifiers_DefaultLibrary]
         , foldMap tokenizeSigmaParam (p : ps)
         , go tN ]
       ASCII_TypeSigmaTuple loc p ps tN -> concat
-        [ mkToken (VarIdent loc "Sigma") SemanticTokenTypes_Class [SemanticTokenModifiers_DefaultLibrary]
+        [ mkToken (VarIdent loc "Sigma") SemanticTokenTypes_Type [SemanticTokenModifiers_DefaultLibrary]
         , foldMap tokenizeSigmaParam (p : ps)
         , go tN ]
       TypeId _loc x a y -> foldMap go [x, a, y]
@@ -164,8 +183,18 @@ tokenizeTerm' varTokenType = go
       Lambda _loc params body -> concat
         [ foldMap tokenizeParam params
         , go body ]
-      Let _loc bind val expr -> concat [tokenizeBind bind, go val, go expr]
-      LetMod _loc _comp bind val expr -> concat [tokenizeBind bind, go val, go expr]
+      t@(Let _ bind val expr) -> concat
+        [ mkKeyword t "let"
+        , tokenizeBind bind
+        , go val
+        , go expr ]
+      t@(LetMod _ comp bind val expr) -> concat
+        [ mkKeyword t "let"
+        , mkModifierBefore comp "mod"
+        , tokenizeModComp comp
+        , tokenizeBind bind
+        , go val
+        , go expr ]
       ASCII_Lambda loc params body -> go (Lambda loc params body)
 
       Pair _loc l r -> foldMap go [l, r]
@@ -200,9 +229,9 @@ tokenizeTerm' varTokenType = go
 
       TypeAsc _loc t type_ -> foldMap go [t, type_]
 
-      ModType _loc _ type_ -> foldMap go [type_]
-      ModApp _loc _ te -> foldMap go [te]
-      ModExtract _loc _ te -> foldMap go [te]
+      ModType _loc md type_ -> tokenizeModality md <> go type_
+      ModApp  _loc md te    -> tokenizeModality md <> go te
+      ModExtract _loc comp te -> tokenizeModComp comp <> go te
 
       RecOrDeprecated{} -> mkToken term SemanticTokenTypes_Regexp [SemanticTokenModifiers_Deprecated]
       TypeExtensionDeprecated{} -> mkToken term SemanticTokenTypes_Regexp [SemanticTokenModifiers_Deprecated]
@@ -236,8 +265,9 @@ tokenizeParamDecl = \case
     , tokenizeTerm cube
     , tokenizeTope tope
     ]
-  ParamTermModalType _loc pat _md type_ -> concat
+  ParamTermModalType _loc pat md type_ -> concat
     [ tokenizeTerm pat
+    , tokenizeModality md
     , tokenizeTerm type_ ]
 
 tokenizeSigmaParam :: SigmaParam -> [SemanticTokenAbsolute]
@@ -245,9 +275,43 @@ tokenizeSigmaParam = \case
   SigmaParam _loc pat type_ -> concat
     [ tokenizePattern pat
     , tokenizeTerm type_ ]
-  SigmaParamModal _loc pat _md type_ -> concat
+  SigmaParamModal _loc pat md type_ -> concat
     [ tokenizePattern pat
+    , tokenizeModality md
     , tokenizeTerm type_ ]
+
+tokenizeModality :: Modality -> [SemanticTokenAbsolute]
+tokenizeModality m = mkToken m SemanticTokenTypes_Modifier []
+
+tokenizeModComp :: ModComp -> [SemanticTokenAbsolute]
+tokenizeModComp = \case
+  Single _loc m      -> tokenizeModality m
+  Comp   _loc m1 m2  -> tokenizeModality m1 <> tokenizeModality m2
+
+mkKeyword :: (HasPosition a) => a -> String -> [SemanticTokenAbsolute]
+mkKeyword x kw = case hasPosition x of
+  Nothing -> []
+  Just (line, col) ->
+    [ SemanticTokenAbsolute
+      { _tokenType      = SemanticTokenTypes_Keyword
+      , _tokenModifiers = []
+      , _line           = fromIntegral line - 1
+      , _startChar      = fromIntegral col  - 1
+      , _length         = fromIntegral (length kw)
+      } ]
+
+mkModifierBefore :: (HasPosition a) => a -> String -> [SemanticTokenAbsolute]
+mkModifierBefore x kw = case hasPosition x of
+  Nothing -> []
+  Just (line, col) ->
+    [ SemanticTokenAbsolute
+      { _tokenType      = SemanticTokenTypes_Modifier
+      , _tokenModifiers = []
+      , _line           = fromIntegral line - 1
+      , _startChar      = fromIntegral col - 1 - fromIntegral (length kw + 1)
+      , _length         = fromIntegral (length kw)
+      } ]
+
 
 mkToken :: (HasPosition a, Print a) => a -> SemanticTokenTypes -> [SemanticTokenModifiers] -> [SemanticTokenAbsolute]
 mkToken x tokenType tokenModifiers =
@@ -257,8 +321,8 @@ mkToken x tokenType tokenModifiers =
       [ SemanticTokenAbsolute
         { _tokenType = tokenType
         , _tokenModifiers = tokenModifiers
-        , _startChar = fromIntegral col - 1    -- NOTE: 0-indexed output for VS Code
-        ,  _line = fromIntegral line - 1             -- NOTE: 0-indexed output for VS Code
+        , _startChar = fromIntegral col - 1
+        ,  _line = fromIntegral line - 1
         ,  _length = fromIntegral $ Prelude.length (printTree x)
         }
         ]
